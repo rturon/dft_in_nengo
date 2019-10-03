1c1f77c40ff2bb0fee28c688a2609ea97e2e6cec
rturon <rturon@uos.de>
2019-10-03 21:15:14 +0200

    moved JSON read functions to python file

---
+++ read_json.py
@@ -0,0 +1,216 @@
+from json import JSONDecoder
+
+def read_JSON_as_list(filename):
+    ''' Reads a JSON file as a list of lists. Necessary for cedar architectures
+        since they contain duplicate keys. 
+
+        Parameters
+        ----------
+        filename: str
+            Path to the JSON file.
+
+        Returns
+        -------
+        list
+            Content of JSON file as list of lists.
+    '''
+    def parse_pairs(pairs):
+        return pairs
+
+    decoder = JSONDecoder(object_pairs_hook=parse_pairs)
+    with open(filename) as json_file:
+        file_content = json_file.read()
+        data = decoder.decode(file_content)
+        
+    return data
+
+
+def fetch_objects(json_as_list):
+    ''' Fetches all objects of a cedar architecture, both the ones outside
+        groups and the one inside groups. For the ones inside groups the group
+        name is added to the object's name to avoid duplicate object names.
+
+        Parameters
+        ----------
+        json_as_list: list
+            List of lists that contains a cedar architecture as returned by
+            read_JSON_as_list. 
+
+        Returns
+        -------
+        list
+            List of all objects/elements of a cedar architecture with their 
+            parameters. 
+    '''
+    # first get the objects outside of groups
+    objects = json_as_list[1][1]
+    
+    key_to_pos_dict = {}
+    for ind, elem in enumerate(json_as_list):
+        key_to_pos_dict[elem[0]] = ind
+        
+    groups = json_as_list[key_to_pos_dict['groups']][1]
+    
+    for group in groups:
+        for obj in group[1][1][1]:
+            # add the group name to the object, otherwise there might be duplicate object names
+            # also added in connections
+            obj[1][0] = ('name', group[0] + "." + obj[1][0][1])
+            objects.append(obj)
+            
+    return objects
+
+
+def fetch_connections(json_as_list):
+    ''' Fetches all connections of a cedar architecture, replacing connections
+        that contain group input or group output nodes by direct connections
+        from their sources to their targets. 
+
+        Parameters
+        ----------
+        json_as_list: list
+            List of lists that contains a cedar architecture as returned by
+            read_JSON_as_list.
+
+        Returns
+        -------
+        list
+            List of all connections, both inside and outside of groups with 
+            connections to and from group inputs and outputs replaced by direct
+            connections. 
+    '''
+    ##########################################################################
+    ####################### helper functions ################################
+    def add_group_name(group_connection, group_name):
+        new_group_connection = [('source', group_name + "." + group_connection[0][1]),
+                                ('target', group_name + "." + group_connection[1][1])]
+        return new_group_connection
+
+    def find_connector_connections(connectors, 
+                                outside_group_connections, 
+                                inside_group_connections,
+                                output_nodes=True):
+        ''' outside_group_connections list of connections, inside_group_connections
+            dictionary of list of connections per group.
+        
+            output_nodes True means that the connectors in the list are output
+            nodes of their groups, if it is False they are input nodes.
+            Elements in connectors should be of the form: 
+            (group_name, connector_name)
+        '''
+        connector_connections = {}
+
+        for connector in connectors:
+            connector_connections[connector] = {'input connections': [], 
+                            'output connections': []}
+
+            if output_nodes:
+                source_string = connector[0] + "." + connector[1]
+                target_string = connector[0] + "." + connector[1] + ".input"
+            else:
+                source_string = connector[0] + "." + connector[1] + ".output"
+                target_string = connector[0] + "." + connector[1] 
+
+            # need the ":", otherwise removing and iterating at the same time leads to 
+            # weird results
+            for connection in outside_group_connections[:]:
+                if connection[0][1] == source_string:
+                    connector_connections[connector]['output connections'].append(connection) 
+                    outside_group_connections.remove(connection)
+                elif connection[1][1] == target_string:
+                    connector_connections[connector]['input connections'].append(connection)
+                    outside_group_connections.remove(connection)
+
+            for connection in inside_group_connections[connector[0]][:]:
+                if connection[1][1] == target_string:
+                    connector_connections[connector]['input connections'].append(connection)
+                    inside_group_connections[connector[0]].remove(connection)
+                elif connection[0][1] == source_string:
+                    connector_connections[connector]['output connections'].append(connection)
+                    inside_group_connections[connector[0]].remove(connection)
+                    
+        return connector_connections
+
+    def replace_middle_connection(middle_connection_dict):
+        ''' takes as input a dictionary with input connections to a node and 
+            output connections from the node and creates new connections substituting
+            the middle node and connecting the inputs from the input nodes and the 
+            outputs from the output nodes directly.
+        '''
+        # each input has to be connected to each output
+        connections_list = []
+
+        # go through all inputs
+        for inp_connection in middle_connection_dict['input connections']:
+            # source at position 0
+            source = inp_connection[0]
+            # go through all outputs
+            for out_connection in middle_connection_dict['output connections']:
+                # target at position 1
+                target = out_connection[1]
+                connections_list.append([source, target])
+
+        return connections_list
+    ##########################################################################
+    ##########################################################################
+
+    key_to_pos_dict = {}
+    for ind, elem in enumerate(json_as_list):
+        key_to_pos_dict[elem[0]] = ind
+        
+    connections_outside_groups = json_as_list[key_to_pos_dict["connections"]][1]
+    groups = json_as_list[key_to_pos_dict["groups"]][1]
+
+    # get the connectors (input and output nodes of groups) and all group connections
+    connectors = {}
+    group_connections = {}
+
+    for group in groups:
+        # get the group name
+        group_name = group[0]
+        key_to_pos_dict_group = {}
+        for ind, elem in enumerate(group[1]):
+            key_to_pos_dict_group[elem[0]] = ind
+        # get the connectors of this group
+        connectors[group_name] = group[1][key_to_pos_dict_group["connectors"]][1]
+        # get the connections of this group and add the group name to the 
+        # object's name
+        add_group_name_lambda = lambda x: add_group_name(x, group_name)
+        group_connections_wn = list(map(add_group_name_lambda, group[1][key_to_pos_dict_group["connections"]][1]))
+        group_connections[group_name] = group_connections_wn
+
+    # divide connectors into input and output nodes
+    group_outputs = []
+    group_inputs = []
+
+    for group in groups:
+        for connector in connectors[group[0]]:
+            if connector[1] == 'true':
+                group_inputs.append((group[0], connector[0]))
+            else:
+                group_outputs.append((group[0], connector[0]))
+
+    # get all connections to and from group output nodes
+    group_output_connections = find_connector_connections(group_outputs, 
+                                                          connections_outside_groups,
+                                                          group_connections)
+    # replace source -> group_output -> target connections by source -> target connections
+    replaced_connections = list(map(replace_middle_connection, 
+                                    group_output_connections.values()))
+    # add the replaced connections to the connection list
+    _ = [connections_outside_groups.extend(cons) for cons in replaced_connections]
+
+    # now do the same for the group input nodes
+    group_input_connections = find_connector_connections(group_inputs, 
+                                                          connections_outside_groups,
+                                                          group_connections,
+                                                          output_nodes=False)
+    replaced_connections = list(map(replace_middle_connection,
+                                    group_input_connections.values()))
+    _ = [connections_outside_groups.extend(cons) for cons in replaced_connections]
+
+    # finally add the connections inside the groups now that all connections
+    # to group input and output nodes have been removed
+    _ = [connections_outside_groups.extend(cons) for cons in group_connections.values()]
+
+    return connections_outside_groups
